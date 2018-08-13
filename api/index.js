421b96d9f421cfbb70c597e27f558fd183a538e3
Erik Macias <erikmacias.ruezga@gmail.com>
2018-08-13 12:24:18 -0500

    Added CORS

--- api/index.js
+++ api/index.js
@@ -15,11 +15,6 @@ var auth = require('../interceptors/auth');
  * Define router
  */
 var router = express.Router();
-/*router.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, *");
-    next();
-});*/
 router.use(cors());
 
 /**
