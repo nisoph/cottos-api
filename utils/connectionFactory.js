ea268ca6e62e201093ddab1fffd8f5e2730f3c27
Erik Macias <erikmacias.ruezga@gmail.com>
2018-09-05 22:13:02 -0500

    added api to add new coto

--- utils/connectionFactory.js
+++ utils/connectionFactory.js
@@ -15,17 +15,5 @@ const pool = mysql.createPool({
   connectionLimit: 10,
 });
 
-/* const pool = mysql.createPool({
-  host: 'us-cdbr-iron-east-01.cleardb.net',
-  port: 3306,
-  user: 'b61bf5ce585674',
-  password: '5e9ff621',
-  database: 'heroku_328778fabfa24e4',
-  timezone: '+05:30',
-  supportBigNumbers: true,
-  dateStrings: true,
-  connectionLimit: 10,
-});*/
-
 // export the module
 module.exports = pool;
